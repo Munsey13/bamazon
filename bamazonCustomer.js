ba893ec6eb8418fdc03ed5183f43866774d354bf
MM13 <Fastsoccer13@gmail.com>
2019-12-09 17:12:07 -0500

    made a function messagesToAsk with a inquirer.prompt that aask two questions. My .then function that has a variable with two if statements and one else statement. The if's and else statements still need some work.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     password: process.env.PW_ID,
     database: "bamazon_db"
 });
-
+//Connecting the mysql server and sql database
 connection.connect(function(err){
     if (err) throw err;
     console.log("Is Connected!");
@@ -19,7 +19,7 @@ connection.connect(function(err){
 
 // function startApp()
 
-//a function that connects to that database and  console.logs all the information in the products table on the database then ends the connection.
+//a function that connects to that database and grabs all the information in the products table on the database
 
 function displayItems(){
     connection.query("SELECT * FROM products", function(err,res){
@@ -28,5 +28,55 @@ function displayItems(){
         console.log("\nID: " + res[i].id + " | " + "Product Name: " + res[i].product_name + " | " + "Department Name: " + res[i].department_name + " | " + "Price(US dollar): $" + res[i].price + " | " + "Stock Quantity: " + res[i].stock_quantity);
         };
         //connection.end();
-    }
-    )}; 
+        messagesToAsk();
+    });
+} 
+
+function messagesToAsk(){
+    connection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err;
+    
+    inquirer
+        .prompt([
+            {
+                name: "ID",
+                type: "rawlist",
+                choices: function(){
+                    let choiceArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                    choiceArray.push(res[i].id);
+                }
+                return choiceArray;
+            },
+                message: "What is the ID of the product you would like to buy?"
+            },
+            {
+                name: "Units",
+                type: "input",
+                message: "How many units of the product would you like to buy?"
+            }    
+
+
+        ])
+        .then(function(answer){
+            let chosenID;
+            for (let i = 0; i < res.length; i++){
+            if(answer.ID === res[i].id){
+                chosenID = res[i];
+              //  console.log("This is working");
+            }
+        }
+            if (parseInt(answer.Units) < chosenID.stock_quantity) {
+                console.log("Your order has been receieved");
+                let newStock = chosenID.stock_quantity - parseInt(answer.Units);
+                console.log(newStock);
+            } else{
+                console.log("Insufficient quantity! Please enter ID and Stock Quantity");
+            }
+            
+            
+        });
+        
+        
+    });
+}
