63844bc56f29c2d41b21eedf66f5cb7730fba9f5
MM13 <Fastsoccer13@gmail.com>
2019-12-14 12:58:21 -0500

    created a js file named bamazonManager, it prompts you and you can chose what you would like to do. Half of the functions are working for the choices you have to chose from in the .prompt, still need to make functions for Add new product and finish Add to inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -26,7 +26,8 @@ inquirer
             "View products for sale",
             "View low inventory",
             "Add to inventory",
-            "Add new product"
+            "Add new product",
+            "Exit"
     ]
     })
     .then(function(answer) {
@@ -35,10 +36,14 @@ inquirer
                 viewProduct();
                 break;
             case "View low inventory":
+                viewLowInventory();
                 break;
             case "Add to inventory":
+                addInventory();
                 break;
             case "Add new product":
+                break;
+            case "Exit":
                 break;
             default:
                 console.log("Unrecognize input, please try again");
@@ -56,7 +61,98 @@ function viewProduct(){
         for (let i = 0; i < res.length; i++) {
             console.log("\nID: " + res[i].id + " | " + "Product Name: " + res[i].product_name + " | " + "Department Name: " + res[i].department_name + " | " + "Price(US dollar): $" + res[i].price + " | " + "Stock Quantity: " + res[i].stock_quantity);
         };
-        //startProgram();
     });
-    startProgram();
+    backToMenu();
+};
+function viewLowInventory() {
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++){
+            if (res[i].stock_quantity < 10){
+                console.log(res[i]);
+            }
+        }
+    });
+    backToMenu();
+};
+
+function addInventory() {
+    inquirer
+        .prompt([{
+
+            name: "Quantity",
+            type: "input",
+            message: "How much inventory would you like to add?",
+            validate: function(val){
+                if (isNaN(val) === false){
+                    return true;
+                } else {
+                    console.log("\nNot a acceptable input. Pleas enter a number.");
+                }
+            }
+        },
+        {
+            name: "id",
+            type: "input",
+            message: "What is the ID number you would like to add the inventory to?",
+            validate: function(val){
+                if (isNaN(val) === false){
+                    return true;
+                } else {
+                    console.log("\nNot a acceptable input. Please enter a number.");
+                    return false;
+                }
+
+            }
+        }
+
+        
+    ])
+    .then(function (answer){
+        
+        
+        let resID = parseInt(answer.id);
+        let resQuant = parseInt(answer.Quantity);
+        console.log("\nSecond response answers" + resID, resQuant);
+        connection.query("UPDATE products SET ? WHERE ?",
+    [
+        {
+            stock_quantity: resQuant
+        },
+        {
+            id: resID
+        }
+    ],
+    
+    function (err){
+        console.log("3rd test this function");
+        if (err) throw err;
+        
+        else {
+            console.log("Inventory Successfully Added!");
+            viewProduct();
+        }
+    }
+    )
+    });
+};
+
+function backToMenu() {
+    inquirer
+        .prompt({
+            name:"Menu",
+            type: "confirm",
+            message: "Back to main menu?"
+        })
+        .then(function (answer){
+            if (answer.Menu === true){
+                startProgram();
+            } else {
+                exit();
+            }
+       });
+};
+
+function exit() {
+    connection.end();
 };
